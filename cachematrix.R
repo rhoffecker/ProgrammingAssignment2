b88bdd1ebc9cae65b566a4c2df38a67574e503c9
Rich Hoffecker <rhoffecker@users.noreply.github.com>
2014-05-25 13:48:32 -0700

    Update cachematrix.R
    Added additional comments to the code explaining what is happening.

--- cachematrix.R
+++ cachematrix.R
@@ -19,13 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
 # Enables the original matrix 'x' to be called
 	get <- function() x
 
-# Solves for the inverse of 'x' and returns as 'm' 
+# Solves for the inverse of 'x' and makes available  as 'm' 
 	setSolve <- function(solve) m <<- solve
 
 # Enables the solved matrix 'm' to be called
 	getSolve <- function() m
 
-# Returns a list of the available functions
+# Makes available a list of the available functions
 	list(set = set, get = get,
 		setSolve = setSolve,
 		getSolve = getSolve)
@@ -37,15 +37,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## of 'x' is solved and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Uses the getSolve function to populate 'm'
 	m <- x$getSolve()
+## If matrix 'm' is in cache, returns matrix 'm'	
 	if(!is.null(m)) {
 		message("getting cached data")
 		return(m)
 	}
+## Since matrix 'm' was not in cache, gets matrix 'x' solves for its inverse
 	data <- x$get()
 	m <- solve(data, ...)
 	x$setSolve(m)
+## Returns matrix 'm'
 	m
 
 }
