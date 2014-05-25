6b7810acde474237ec279a756e2d1b6f7882188d
Rich Hoffecker <rhoffecker@users.noreply.github.com>
2014-05-25 13:57:18 -0700

    Update cachematrix.R
    Additional comment cleanup.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The assignment was to create functions used for creating an inverse 
+## of a sqare matrix. There are two primary functions in this 
+## assibnment: makeCacheMatrix and cacheSolve.
 
-## The makeCacheMatrix function identifies the functions
+## The makeCacheMatrix function creates the functions
 ## that are used to solve for the inverse of a matrix. The output
 ## is a list of these functions.
 
@@ -31,7 +32,6 @@ makeCacheMatrix <- function(x = matrix()) {
 		getSolve = getSolve)
 }
 
-
 ## The cacheSolve function determines if the inverse of 'x' has been 
 ## created, is so it returns the matrix from cache; if not the inverse
 ## of 'x' is solved and returned.
@@ -39,16 +39,18 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
 ## Uses the getSolve function to populate 'm'
 	m <- x$getSolve()
+	
 ## If matrix 'm' is in cache, returns matrix 'm'	
 	if(!is.null(m)) {
 		message("getting cached data")
 		return(m)
 	}
+	
 ## Since matrix 'm' was not in cache, gets matrix 'x' solves for its inverse
 	data <- x$get()
 	m <- solve(data, ...)
 	x$setSolve(m)
+	
 ## Returns matrix 'm'
 	m
-
 }
